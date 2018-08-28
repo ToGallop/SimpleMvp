db7c07f2882725e6ec3f458873ff46cdf1f200b8
ToGallop <n.zjx@163.com>
2018-08-28 21:09:39 +0800

    项目首次提交

---
+++ app/src/main/java/com/togallop/simplemvp/MainActivity.java
@@ -0,0 +1,17 @@
+package com.togallop.simplemvp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * Created by DELL on 2018年8月28日 028
+ * Describe:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
