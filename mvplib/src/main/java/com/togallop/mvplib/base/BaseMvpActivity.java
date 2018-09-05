4ed59fc4958d32305e36dac0d24869b926c68d62
ToGallop <n.zjx@163.com>
2018-09-05 09:14:16 +0800

    baseactivity修改

--- mvplib/src/main/java/com/togallop/mvplib/base/BaseMvpActivity.java
+++ mvplib/src/main/java/com/togallop/mvplib/base/BaseMvpActivity.java
@@ -1,6 +1,5 @@
 package com.togallop.mvplib.base;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.util.ArraySet;
